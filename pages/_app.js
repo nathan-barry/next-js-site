5bf05112312bdabafb9825100d0f7d61e613e1ae
Nathan Barry <nathanbarry@protonmail.com>
2022-07-02 12:46:59 -0500

    change responsiveness again

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="w-full h-full p-12 sm:p-16 md:p-28 lg:px-64 xl:px-96 2xl:px-128 max-w-2000">
+    <div className="w-full h-full p-8 sm:p-16 md:p-28 lg:px-64 xl:px-96 2xl:px-128 max-w-2000">
       <Header />
       <Component {...pageProps} />
     </div>
