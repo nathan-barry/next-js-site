11e8c6e2375d8e5ed92253573770ad37f374b5f2
Nathan Barry <nathanbarry@protonmail.com>
2022-07-02 17:22:30 -0500

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import "../styles/globals.css";
 import Header from "../components/Header";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const isProd = process.env.NODE_ENV === "production";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (isProd) {
+        window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
+          page_path: url,
+        });
+      }
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div className="w-full h-full px-8 pt-12 pb-16 sm:p-16 md:p-28 lg:px-64 xl:px-96 2xl:px-128 max-w-2000">
       <Header />
