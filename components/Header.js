7c7bac99b9fd313692bf938faf020aef3333ee31
Nathan Barry <nathanbarry@protonmail.com>
2022-07-02 15:38:35 -0500

    reversed change

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,7 @@ export default function Header() {
   return (
     <header>
       <Link href="/">
-        <h1 className="mb-16 ">
-          <span className="hover:text-hover">nathan.rs</span>
-        </h1>
+        <h1 className="mb-16 hover:text-hover">nathan.rs</h1>
       </Link>
     </header>
   );
--- components/Main.js
+++ components/Main.js
@@ -30,53 +30,33 @@ export default function Main() {
 
       <p>Currently:</p>
       <Link href="https://friktion.fi/">
-        <p>
-          <span className="hover:text-hover">
-            - Rust Developer at Friktion Labs
-          </span>
-        </p>
+        <p className="hover:text-hover">- Rust Developer at Friktion Labs</p>
       </Link>
       <br></br>
 
       <p>Previously:</p>
       <Link href="https://bean.money/">
-        <p>
-          <span className="hover:text-hover">
-            - Solidity Developer at Beanstalk
-          </span>
-        </p>
+        <p className="hover:text-hover">- Solidity Developer at Beanstalk</p>
       </Link>
       <Link href="https://www.rubicon.finance/">
-        <p>
-          <span className="hover:text-hover">
-            - Full-Stack Developer at Rubicon Finance
-          </span>
+        <p className="hover:text-hover">
+          - Full-Stack Developer at Rubicon Finance
         </p>
       </Link>
       <Link href="https://www.texasblockchain.org/">
-        <p>
-          <span className="hover:text-hover">
-            {`- VP of UT's Blockchain Club`}
-          </span>
-        </p>
+        <p className="hover:text-hover">{`- VP of UT's Blockchain Club`}</p>
       </Link>
       <Link href="https://www.zfellows.com/">
-        <p>
-          <span className="hover:text-hover">- Z Fellows Apprentice</span>
-        </p>
+        <p className="hover:text-hover">- Z Fellows Apprentice</p>
       </Link>
       <br></br>
 
       <p>More:</p>
       <Link href="https://github.com/0xnathanbarry">
-        <p>
-          <span className="hover:text-hover">- Github</span>
-        </p>
+        <p className="hover:text-hover">- Github</p>
       </Link>
       <Link href="https://www.linkedin.com/in/nathan-barry-31b6ba220/">
-        <p>
-          <span className="hover:text-hover">- Linkedin</span>
-        </p>
+        <p className="hover:text-hover">- Linkedin</p>
       </Link>
       <br></br>
 
@@ -84,9 +64,7 @@ export default function Main() {
       <p>- Arch linux and i3vm ethusiast, avid vim user</p>
       <p>- 4:28 mile, 245lb bench, 15:56 5k</p>
       <Link href="/me.jpeg">
-        <p>
-          <span className="hover:text-hover">- Very handsome :)</span>
-        </p>
+        <p className="hover:text-hover">- Very handsome :)</p>
       </Link>
       <br></br>
 
