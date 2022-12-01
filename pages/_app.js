3ca6de7d090749dd76ddac575e049f975190970a
Nathan Barry <nathanbarry@protonmail.com>
2022-12-01 16:04:24 -0600

    google analytics test

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,25 @@ import "../styles/globals.css";
 import Header from "../components/Header";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
-        page_path: url,
-      });
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <div className="w-full h-full px-8 pt-12 pb-16 sm:p-16 md:p-28 lg:px-64 xl:px-96 2xl:px-128 max-w-2000">
       <Header />
       <Component {...pageProps} />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-1Y735N717D"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1Y735N717D');
+        `}
+      </Script>
     </div>
   );
 }
